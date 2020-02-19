a139015586e683044aef2b92b4c77ba55fec4f99
omriyaacobi <55637769+omriyaacobi@users.noreply.github.com>
2020-02-19 10:39:04 +0200

    Add files via upload

---
+++ omrihameleh.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal f1 = new Fish();
+             f1;//downcasting
+            Fish s = new Fish(1,"sting","string",3);//not legel
+            Animal a = s;//auto upcast the fish
+            Fish s1 = (Fish)a;
+            Console.WriteLine(s1.ToString()); 
+            Console.ReadLine();
+        }
+    }
+}
